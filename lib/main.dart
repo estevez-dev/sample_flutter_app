6d8254dbb0f87927c9de1770f4fa8431613e4660
Yegor Vialov <vyalov.egor@gmail.com>
2020-03-18 10:34:10 +0000

    geolocator in a separate isolate

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,48 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:workmanager/workmanager.dart' as workManager;
+import 'package:geolocator/geolocator.dart';
+import 'package:battery/battery.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(SampleApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SampleApp extends StatefulWidget {
+  @override
+  _SampleAppState createState() => new _SampleAppState();
+  
+}
+
+class _SampleAppState extends State<SampleApp> {
+
+  @override
+  void initState() {
+    workManager.Workmanager.initialize(
+      updateDeviceLocation,
+      isInDebugMode: false
+    );
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
+
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +51,143 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _msg = 'Background task not started yet';
 
-  void _incrementCounter() {
+  void _startBackgroudTask() async {
+    await workManager.Workmanager.registerPeriodicTask(
+          "sampleBackgroundTask",
+          "sampleBackgroundTask-01",
+          inputData: {
+            "webhookId": 'jsgiuerngierjg',
+            "httpWebHost": 'example.com'
+          },
+          frequency: Duration(minutes: 20),
+          existingWorkPolicy: workManager.ExistingWorkPolicy.replace,
+          backoffPolicy: workManager.BackoffPolicy.linear,
+          backoffPolicyDelay: Duration(minutes: 20),
+          constraints: workManager.Constraints(
+            networkType: workManager.NetworkType.connected,
+          ),
+        );
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _msg = 'Background task started. Tap the button.';
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'This is the message:',
             ),
             Text(
-              '$_counter',
+              '$_msg',
               style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _startBackgroudTask,
+        tooltip: 'Start',
+        child: Icon(Icons.launch),
+      ),
     );
   }
+}
+
+void updateDeviceLocation() {
+  workManager.Workmanager.executeTask((backgroundTask, data) async {
+    print("[Background $backgroundTask] Started");
+    Geolocator geolocator = Geolocator();
+    var battery = Battery();
+    String webhookId = data["webhookId"];
+    String httpWebHost = data["httpWebHost"];
+    String logData = '==> ${DateTime.now()} [Background $backgroundTask]:';
+    print("[Background $backgroundTask] Getting path for log file...");
+    final logFileDirectory = await getExternalStorageDirectory();
+    print("[Background $backgroundTask] Opening log file...");
+    File logFile = File('${logFileDirectory.path}/sample-background-log.txt');
+    print("[Background $backgroundTask] Log file path: ${logFile.path}");
+    if (webhookId != null && webhookId.isNotEmpty) {
+      String url = "$httpWebHost/api/webhook/$webhookId";
+      Map<String, String> headers = {};
+      headers["Content-Type"] = "application/json";
+      Map data = {
+        "type": "update_location",
+        "data": {
+          "gps": [],
+          "gps_accuracy": 0,
+          "battery": 100
+        }
+      };
+      print("[Background $backgroundTask] Getting battery level...");
+      int batteryLevel;
+      try {
+        batteryLevel = await battery.batteryLevel;
+        print("[Background $backgroundTask] Got battery level: $batteryLevel");
+      } catch(e) {
+        print("[Background $backgroundTask] Error getting battery level: $e. Setting zero");
+        batteryLevel = 0;
+        logData += 'Battery: error, $e';
+      }
+      if (batteryLevel != null) {
+        data["data"]["battery"] = batteryLevel;
+        logData += 'Battery: success, $batteryLevel';
+      } else {
+        logData += 'Battery: error, level is null';
+      }
+      Position location;
+      try {
+        location = await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high, locationPermissionLevel: GeolocationPermission.locationAlways);
+        if (location != null && location.latitude != null) {
+          logData += ' || Location: success, ${location.latitude} ${location.longitude} (${location.timestamp})';
+          data["data"]["gps"] = [location.latitude, location.longitude];
+          data["data"]["gps_accuracy"] = location.accuracy;
+          try {
+            http.Response response = await http.post(
+                url,
+                headers: headers,
+                body: json.encode(data)
+            );
+            if (response.statusCode >= 200 && response.statusCode < 300) {
+              logData += ' || Post: success, ${response.statusCode}';
+            } else {
+              logData += ' || Post: error, ${response.statusCode}';
+            }
+          } catch(e) {
+            logData += ' || Post: error, $e';
+          }
+        } else {
+          logData += ' || Location: error, location is null';
+        }
+      } catch (e) {
+        print("[Background $backgroundTask] Location error: $e");
+        logData += ' || Location: error, $e';
+      }
+    } else {
+      logData += 'Not configured';
+    }
+    print("[Background $backgroundTask] Writing log data...");
+    try {
+      var fileMode;
+      if (logFile.existsSync() && logFile.lengthSync() < 5000000) {
+        fileMode = FileMode.append;
+      } else {
+        fileMode = FileMode.write;
+      }
+      await logFile.writeAsString('$logData\n', mode: fileMode);
+    } catch (e) {
+      print("[Background $backgroundTask] Error writing log: $e");
+    }
+    print("[Background $backgroundTask] Finished.");
+    return true;
+  });
 }
